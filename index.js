0d0235bbf02f4b22c68b1f1c9877bfde142fb3c9
Jeff <jeffreyRstocker@gmail.com>
2017-12-16 11:02:36 -0800

    Added some endpoints to server

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 var env = require('dotenv').config()
 var express = require('express');
 var app = express();
+var port = process.env.port || 8080;
+var elastic = require ('./elasticsearch/elasticsearch.js')
 
-app.use ('/withdraw/:user', () => {
+
+
+app.post ('/withdraw/:userID', () => {
+  userID = req.params.userID
+})
+
+app.post ('/cashout/:userID', () => {
+  userID = req.params.userID
+})
+
+
+app.get ('/users/banks', () => {
 
 })
 
-app.use ('/cashout/:user', () => {
 
+
+
+var server = app.listen(8080, () =>  {
+  // console.log("... port %d in %s mode", app.address().port, app.settings.env);
+  console.log('App is listening on port: ', port)
+})
+
+
+process.on('uncaughtException', (error) => {
+  console.log ('UncaughtError!!', error)
 })
