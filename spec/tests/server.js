8cec1d92f373c7cf8269c65bb4fea11e0cafe14f
Jeff <jeffreyRstocker@gmail.com>
2017-12-16 11:20:10 -0800

    setup testing

---
+++ spec/tests/server.js
@@ -0,0 +1,11 @@
+var chai = require('chai')
+var assert = require('assert');
+var expect = require('chai').expect;
+var should = chai.should();
+
+describe('test' ,() => {
+  it ('should be 5', () => {
+    expect(5).to.be.equal(5)
+  })
+})
+
