4026c19677b22d03e5896e6dce82b4b56194c95c
Jeff <jeffreyRstocker@gmail.com>
2017-12-16 13:49:55 -0800

    Used Consumer to ping the queue

--- index.js
+++ index.js
@@ -2,24 +2,76 @@ var env = require('dotenv').config()
 var express = require('express');
 var app = express();
 var port = process.env.port || 8080;
-var elastic = require ('./elasticsearch/elasticsearch.js')
+// var elastic = require ('./elasticsearch/elasticsearch.js')
+var Consumer = require('sqs-consumer');
+var AWS = require('aws-sdk');
+AWS.config.update({accessKeyId: process.env.AWS_PUBLIC_KEY, secretAccessKey: process.env.AWS_SECRET_KEY});
 
 
-
-app.post ('/withdraw/:userID', () => {
-  userID = req.params.userID
+var sqs = new AWS.SQS({
+  region: 'us-east-2',
+  credentials: process.env.AWS_PUBLIC_KEY
 })
 
-app.post ('/cashout/:userID', () => {
-  userID = req.params.userID
-})
+var sendMessage = function (message, callback) {
+  var msg = {payload: message};
+  var params = {
+    MessageBody: JSON.stringify(msg),
+    QueueUrl: process.env.SQS_URL
+  }
+  sqs.sendMessage(params, callback)
+}
+
+// sendMessage('test', (err, data) => {
+//   if (err) {
+//     console.log('ERR', err);
+//   } else {
+//     console.log(data);
+//   }
+// })
+
+var getData = function () {
+  console.log('runing')
+  var request = Consumer.create({
+    queueUrl: process.env.SQS_URL,
+    region: 'us-east-2',
+    batchSize: 10,
+    handleMessage: function (message, done) {
+      var msgBody = JSON.parse(message.Body);
+      console.log(msgBody);
+      return done();
+    }
+  });
+  request.on('error', err => {
+    console.log('error', err)
+  })
+  request.start();
+
+  request.on('error', function (err) {
+    console.log(err);
+  });
+  // request.on('empty', function () {
+    // console.log('queue is empty')
+  // })
+}
+getData()
+
+
+
+// app.post ('/withdraw/:userID', () => {
+//   userID = req.params.userID
+// })
+
+// app.post ('/cashout/:userID', () => {
+//   userID = req.params.userID
+// })
 
 
 app.get ('/users/banks', () => {
 
 })
 
-
+var sqsURL = 'https://sqs.us-east-2.amazonaws.com/722156248668/inputToBankServices'
 
 
 var server = app.listen(8080, () =>  {
