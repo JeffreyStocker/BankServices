f788b78ff504f568c51f06ec7b34723b3c1466f9
Jeff <jeffreyRstocker@gmail.com>
2017-12-16 14:41:20 -0800

    added a memcache

---
+++ memcache/cache.js
@@ -0,0 +1,21 @@
+const Memcached = require('memcached');
+
+var options = {};
+var memcached = new Memcached(process.env.MEM_CACHE, options);
+
+memcached.set('test', 'here', 20, function (err, succ) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('sucess', succ)
+
+  }
+})
+
+memcached.get('test', (err, data) =>  {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('data', data)
+  }
+})
