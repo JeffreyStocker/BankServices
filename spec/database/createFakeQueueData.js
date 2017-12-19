117ce2b515efe557cf30e535b09ab80cc709836f
Jeff <jeffreyRstocker@gmail.com>
2017-12-19 12:29:42 -0800

    Refactored: createFakeQueueData to send less messages at once and to again use the real sqs

--- spec/database/createFakeQueueData.js
+++ spec/database/createFakeQueueData.js
@@ -1,24 +1,28 @@
 var AWS = require('aws-sdk');
 const env = require('dotenv').config();
+// AWS.config.update({accessKeyId: process.env.AWS_PUBLIC_KEY, secretAccessKey: process.env.AWS_SECRET_KEY});
 AWS.config.update({accessKeyId: process.env.AWS_PUBLIC_KEY, secretAccessKey: process.env.AWS_SECRET_KEY});
 
 var sqsURL = 'https://sqs.us-east-2.amazonaws.com/722156248668/inputToBankServices'
+// var sqsURL = 'http://localhost:1000/722156248668/inputToBankServices'
 
 var sqs = new AWS.SQS({
   region: 'us-east-2',
 })
 
 module.exports.sendMessageToQueue = function (message, callback) {
-  var msg = {payload: message};
+  // var msg = {payload: message};
+  var msg = message;
   var params = {
     MessageBody: JSON.stringify(msg),
-    QueueUrl: process.env.SQS_URL
+    // QueueUrl: process.env.SQS_URL
+    QueueUrl: sqsURL
   }
   sqs.sendMessage(params, callback)
 }
 
 
-for (var i = 0; i < 30; i ++) {
+for (var i = 0; i < 1; i ++) {
   var data = {
     transactionID: Math.floor(Math.random()*200000),
     route: 'cashout',
