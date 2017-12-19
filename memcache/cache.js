0b1ba42fa4afd94ba157a797809f92706a762a38
Jeff <jeffreyRstocker@gmail.com>
2017-12-18 16:40:55 -0800

    Converted cached.js to promises and exported them

--- memcache/cache.js
+++ memcache/cache.js
@@ -3,19 +3,34 @@ const Memcached = require('memcached');
 var options = {};
 var memcached = new Memcached(process.env.MEM_CACHE, options);
 
-memcached.set('test', 'here', 20, function (err, succ) {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('memcache:', succ)
+var set = function (key, value, timeout = 20) {
+  return new Promise ((resolve, revoke) => {
+    memcached.set(key, value, timeout, function (err, succ) {
+      if (err) {
+        // console.log(err)
+        revoke (err)
+      } else {
+        // console.log('memcache:', succ)
+        resolve(succ)
+      }
+    })
+  })
+}
 
-  }
-})
 
-memcached.get('test', (err, data) =>  {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('data', data)
-  }
-})
+var get = function (key) {
+  return new Promise ((resolve, revoke) => {
+    memcached.get(key, (err, data) =>  {
+      if (err) {
+        revoke (err)
+        // console.log(err)
+      } else {
+        resolve(data)
+        // console.log('data', data)
+      }
+    })
+  })
+}
+
+module.exports.set = set;
+module.exports.get = get;
