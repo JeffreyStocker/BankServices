f76ac10a81698ad09927e524bc1fa70bc89f20a5
Jeff <jeffreyRstocker@gmail.com>
2017-12-19 12:32:07 -0800

    added some index error catching to createIndex in elasticSearch

--- elasticsearch/elasticsearch.js
+++ elasticsearch/elasticsearch.js
@@ -2,7 +2,7 @@ var elasticsearch=require('elasticsearch');
 //https://www.compose.com/articles/getting-started-with-elasticsearch-and-node/
 var client = new elasticsearch.Client( {
   hosts: 'localhost:9200',
-  log: 'trace'
+  // log: 'trace'
 });
 
 module.exports.client = client;
@@ -13,12 +13,17 @@ var createIndex = function (name) {
     index: name
   })
 }
-createIndex('log')
-.then(
+
+createIndex('log')  /// already created // uncomment to create
+.then( status => {
   console.log('index complete')
-)
+})
 .catch(err => {
-  console.log('index did not complete')
+  if (err.body.error.type === "resource_already_exists_exception"){
+    console.log('index in elasticsearch already exists')
+  } else {
+    console.log('Index Creation error', err)
+  }
 })
 
 // var mapping = module.exports.mapping = function (index) {
@@ -34,21 +39,21 @@ createIndex('log')
 // }
 // mapping('log')
 
-var ping = module.exports.ping = function () {
-  client.ping({
-    // ping usually has a 3000ms timeout
-    requestTimeout: 1000
-  }, function (error) {
-    if (error) {
-      // console.trace('elasticsearch cluster is down!');
-      console.log('elasticsearch cluster is down!');
-    } else {
-      console.log('All is well');
-    }
-  });
-}
+// var ping = module.exports.ping = function () {
+//   client.ping({
+//     // ping usually has a 3000ms timeout
+//     requestTimeout: 1000
+//   }, function (error) {
+//     if (error) {
+//       // console.trace('elasticsearch cluster is down!');
+//       console.log('elasticsearch cluster is down!');
+//     } else {
+//       console.log('All is well');
+//     }
+//   });
+// }
 
-module.exports.addRecord = function (data) {
+var addRecord = function (data) {
   //https://blog.raananweber.com/2015/11/24/simple-autocomplete-with-elasticsearch-and-node-js/
   return client.index({
     index: 'log',
@@ -58,14 +63,17 @@ module.exports.addRecord = function (data) {
     }
   })
 }
+module.exports.addRecord = addRecord;
 
-module.exports.addRecord('testings')
-.then (data => {
-  console.log('data', data)
-})
-.catch(err => {
-  console.log('err', err)
-})
-// module.exports.addRecord('testings2')
-// module.exports.addRecord('testings1')
-// module.exports.addRecord('testings4342')
+
+
+addRecord('testings')
+  .then (data => {
+    // console.log('data', data)
+  })
+  .catch(err => {
+    // console.log('err', err)
+  })
+  // module.exports.addRecord('testings2')
+  // module.exports.addRecord('testings1')
+  // module.exports.addRecord('testings4342')
