3e118a02b4ad25c708bea21241b81db70c4c6c4a
Jeff <jeffreyRstocker@gmail.com>
2017-12-20 16:52:22 -0800

    Added some mock functions

--- middleware/plaid.js
+++ middleware/plaid.js
@@ -1,16 +1,21 @@
 if (!process.env.PORT) {
   var dotenv= require('dotenv').config();
-  var PLAID_CLIENT_ID=process.env.PLAID_CLIENT_ID;
-  var PLAID_SECRET_KEY=process.env.PLAID_SECRET_KEY;
-  var PLAID_PUBLIC_KEY=process.env.PLAID_PUBLIC_KEY;
-  var PLAID_ENV=process.env.PLAID_ENV;
 }
 var { winston } = require ('../elasticsearch/winston.js')
 var plaid = require('plaid');
 
-var access_token = 'access-sandbox-25194add-267c-4e10-95aa-05fab88dc69f';
+var useFake = process.env.USEFAKE || true;
 
+var PLAID_CLIENT_ID=process.env.PLAID_CLIENT_ID;
+var PLAID_SECRET_KEY=process.env.PLAID_SECRET_KEY;
+var PLAID_PUBLIC_KEY=process.env.PLAID_PUBLIC_KEY;
+var PLAID_ENV=process.env.PLAID_ENV;
+
+var access_token = 'access-sandbox-25194add-267c-4e10-95aa-05fab88dc69f';
+console.log(PLAID_CLIENT_ID)
 const plaidClient = new plaid.Client(PLAID_CLIENT_ID, PLAID_SECRET_KEY, PLAID_PUBLIC_KEY, plaid.environments[PLAID_ENV]);
+
+
 
 var getAuth = function (access_token) {
   return new Promise ((resolve, revoke) => {
@@ -24,6 +29,64 @@ var getAuth = function (access_token) {
   })
 }
 
+
+if (useFake) {
+  getAuth = function () {
+    return new Promise ((resolve, revoke) => {
+      var data =
+      { accounts:
+        [ { account_id: 'JpGN5ELowxTd1LaxGx8JH4pLpjxbV6FpJb3Ve',
+            balances: { available: 100, current: 110, limit: null },
+            mask: '0000',
+            name: 'Plaid Checking',
+            official_name: 'Plaid Gold Standard 0% Interest Checking',
+            subtype: 'checking',
+            type: 'depository' },
+          { account_id: '5lwMgopqnbswXzjlqlJGskGQGdxlaMtzdxlq8',
+            balances: { available: 200, current: 210, limit: null },
+            mask: '1111',
+            name: 'Plaid Saving',
+            official_name: 'Plaid Silver Standard 0.1% Interest Saving',
+            subtype: 'savings',
+            type: 'depository' },
+          { account_id: 'lkXQKZE8mDc4R6ZmXmx1I3MmM5pKxDi17LQDg',
+            balances: { available: null, current: 1000, limit: null },
+            mask: '2222',
+            name: 'Plaid CD',
+            official_name: 'Plaid Bronze Standard 0.2% Interest CD',
+            subtype: 'cd',
+            type: 'depository' },
+          { account_id: 'kleQqyEgMDs9LBjAyAGas5EAEGxoJ1Hrpxny7',
+            balances: { available: null, current: 410, limit: 2000 },
+            mask: '3333',
+            name: 'Plaid Credit Card',
+            official_name: 'Plaid Diamond 12.5% APR Interest Credit Card',
+            subtype: 'credit card',
+            type: 'credit' } ],
+       item:
+        { available_products: [ 'balance' ],
+          billed_products: [ 'auth', 'transactions' ],
+          error: null,
+          institution_id: 'ins_1',
+          item_id: '7KJ5EpqrRbtlR31BrB5JUlBgxLKWrqsgzQXgj',
+          webhook: '' },
+       numbers:
+        [ { account: '1111222233330000',
+            account_id: 'JpGN5ELowxTd1LaxGx8JH4pLpjxbV6FpJb3Ve',
+            routing: '011401533',
+            wire_routing: '021000021' },
+          { account: '1111222233331111',
+            account_id: '5lwMgopqnbswXzjlqlJGskGQGdxlaMtzdxlq8',
+            routing: '011401533',
+            wire_routing: '021000021' } ],
+       request_id: 'Bw7Bw',
+       status_code: 200 }
+       resolve (data);
+    })
+  }
+}
+
+
 var createItem = function () {
   plaidClient.exchangePublicToken('public-sandbox-e3c11412-9417-4ffc-b2c4-e7e67cc9a41a', (err, data) => {
     if (err){
@@ -33,19 +96,6 @@ var createItem = function () {
     }
   });
 }
-
-getAuth(access_token)
-.then(data => {
-  console.log(data.accounts[0].balances)
-  //data.accounts[0].balances = {available, current, limit}
-  console.log(checkIfUserHasAccountsWithEnoughBalance(data, 100))
-})
-.catch(err => {
-  console.log(err)
-});
-
-module.exports.getAuth = getAuth;
-
 
 var checkIfUserHasAccountsWithEnoughBalance = function (infoFromPlaid, amount) {
   if (!infoFromPlaid) { return 'Error'}
@@ -62,16 +112,16 @@ var checkIfUserHasAccountsWithEnoughBalance = function (infoFromPlaid, amount) {
   }
   return false;
 
-//example data from plaid
-//balances = { available: 100, current: 110, limit: null }
-// { accounts:
-//    [ { account_id: 'JpGN5ELowxTd1LaxGx8JH4pLpjxbV6FpJb3Ve',
-//        balances: [Object],
-//        mask: '0000',
-//        name: 'Plaid Checking',
-//        official_name: 'Plaid Gold Standard 0% Interest Checking',
-//        subtype: 'checking',
-//        type: 'depository' },
+  //example data from plaid
+  //balances = { available: 100, current: 110, limit: null }
+  // { accounts:
+  //    [ { account_id: 'JpGN5ELowxTd1LaxGx8JH4pLpjxbV6FpJb3Ve',
+  //        balances: [Object],
+  //        mask: '0000',
+  //        name: 'Plaid Checking',
+  //        official_name: 'Plaid Gold Standard 0% Interest Checking',
+  //        subtype: 'checking',
+  //        type: 'depository' },
 //      { account_id: '5lwMgopqnbswXzjlqlJGskGQGdxlaMtzdxlq8',
 //        balances: [Object],
 //        mask: '1111',
@@ -134,3 +184,23 @@ var checkIfUserHasAccountsWithEnoughBalance = function (infoFromPlaid, amount) {
 // Possible environments: sandbox, development, production
 
 // For example: access-development-7c69d345-hda9-ka68-ahs3-e9317406a2
+
+
+module.exports.getAuth = getAuth;
+module.exports.checkIfUserHasAccountsWithEnoughBalance = checkIfUserHasAccountsWithEnoughBalance;
+
+
+//// test/////
+// getAuth(access_token)
+// .then(data => {
+//   data.accounts.forEach(account => {
+//     console.log(account.balances)
+//   })
+//   console.log(data)
+//   //data.accounts[0].balances = {available, current, limit}
+
+//   console.log(checkIfUserHasAccountsWithEnoughBalance(data, 100))
+// })
+// .catch(err => {
+//   console.log(err)
+// })
