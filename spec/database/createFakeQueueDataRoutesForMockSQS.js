851822fcc4ccf535546d0070155ad44c5677994a
Jeff <jeffreyRstocker@gmail.com>
2017-12-29 12:29:00 -0800

    Updated: Fake sqs queue to a mock sqs server

--- spec/database/createFakeQueueDataRoutesForMockSQS.js
+++ spec/database/createFakeQueueDataRoutesForMockSQS.js
@@ -4,16 +4,23 @@ const env = require('dotenv').config();
 var axios = require('axios');
 // AWS.config.update({accessKeyId: process.env.AWS_PUBLIC_KEY, secretAccessKey: process.env.AWS_SECRET_KEY});
 var url = 'http://localhost:8000/';
+// var url = 'http://sqs-mock.docker:8000/';
 
 
 var createRoute = function (route = 'test') {
-  axios.post ('http://localhost:8000/',
+  axios.post (url,
     `Action=CreateQueue&QueueName=${route}&AWSAccessKeyId=access%20key%20id` )
     .then (status => {
       console.log(status.data);
     })
     .catch (err => {
-      console.log(err.response.status, err.response.statusText);
+      try {
+        console.log(err.response.status, err.response.statusText);
+      } catch (anotherError) {
+        console.log(err.code);
+        // console.log(err.Error);
+        // console.log(err);
+      }
     });
 };
 
--- spec/database/createFakeQueueDataToSqsMock.js
+++ spec/database/createFakeQueueDataToSqsMock.js
@@ -121,19 +121,18 @@ var createRandomString = function (length = 37, lowerCase = false) {
 //   console.log(err.response.status, err.response.statusText);
 // })
 
-for (var i = 0; i < 2000; i ++) {
+for (var i = 0; i < 200; i ++) {
   //"Action=SendMessage&QueueUrl=http%3A%2F%2Fsqs.docker%3A9494%2Ftest-queue&MessageBody=testing123&
-  var message = JSON.stringify(
-    {
-      transactionID: Math.floor((Math.random() * 2147483640) + 10000001),
-      route: Math.floor((Math.random() * 2) + 1) === 1 ? 'cashout' : 'withdraw',
-      userID: Math.floor((Math.random() * 2147483640) + 10000001),
-      amount: Math.floor(Math.random() * 300000) / 100,
-      message: JSON.stringify()
-    }
-  );
   axios.post('http://localhost:8000/inputToBankServices',
-    `Action=SendMessage&QueueUrl=localHost/test&MessageBody=${message}`
+    `Action=SendMessage&QueueUrl=localHost/test&MessageBody=` + JSON.stringify(
+      {
+        transactionID: Math.floor((Math.random() * 2147483640) + 10000001),
+        route: Math.floor((Math.random() * 2) + 1) === 1 ? 'cashout' : 'withdraw',
+        userID: Math.floor((Math.random() * 2147483640) + 10000001),
+        amount: Math.floor(Math.random() * 300000) / 100,
+        message: JSON.stringify()
+      }
+    )
   )
     .then (status => {
       console.log(status.data);
