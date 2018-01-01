0123807a14600bb44029e41cb9da2e69df1217cc
Jeff <jeffreyRstocker@gmail.com>
2017-12-31 19:05:08 -0800

    added needed require modules to actions.js and moved some functions around

--- queues/actions.js
+++ queues/actions.js
@@ -1,33 +1,8 @@
+var { read } = require ('fs');
+var { transactionById } = require('dwolla');
+var { sendMessageToCashoutQueue, sendMessageToTransactionsQueue } = require ('./queue.js')
+var db = require ('../database/databasePG.js');
 var dwolla = require ('../middleware/dwolla.js');
-
-var actionsForBankDeposits = function (data) {
-  console.log('route', actionData.route);
-  dwolla.transferFunds(amount, processorToken)
-    .then (transferResults => {
-    })
-    .catch (err => {
-      console.log(err);
-      try {
-        var id = transactionID;
-      } catch (err) {
-        var id = 'unknown';
-      }
-      winston.warn({
-        transactionID: id,
-        error: err,
-      });
-    });
-
-  if (actionData.route === 'cashout') {
-
-    sendMessageToCashoutQueue(data.transactionID);
-  } else if (actionData.route === 'withdraw') {
-  } else {
-    console.log('no route found');
-  }
-
-};
-
 
 
 var action = function (actionData) {
@@ -73,6 +48,34 @@ var action = function (actionData) {
   } else {
     console.log('no route found');
   }
+};
+
+
+var actionsForBankDeposits = function (data) {
+  console.log('route', actionData.route);
+  dwolla.transferFunds(amount, processorToken)
+    .then (transferResults => {
+      var status;
+      if (transferResults === true) {
+        status = 'confirmed';
+      } else if (transferResults === false) {
+        status = 'cancelled';
+      }
+      db.updateTransactionStatus(transactionById, status)
+      sendMessageToTransactionsQueue(transactionById, status);
+    })
+    .catch (err => {
+      console.log(err);
+      try {
+        var id = transactionID;
+      } catch (err) {
+        var id = 'unknown';
+      }
+      winston.warn({
+        transactionID: id,
+        error: err,
+      });
+    });
 };
 
 
