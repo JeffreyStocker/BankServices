4c7e7d3cbc41ff4559905a07c4783c2ba0654181
Jeff <jeffreyRstocker@gmail.com>
2018-01-02 13:40:09 -0800

    feat: actions.js added some winston logs fixed circular require

--- queues/actions.js
+++ queues/actions.js
@@ -1,13 +1,39 @@
 var { read } = require ('fs');
 var { transactionById } = require('dwolla');
-var { sendMessageToCashoutQueue, sendMessageToTransactionsQueue } = require ('./queue.js')
+var { sendMessageToCashoutQueue } = require('./queue');
+var sendMessageToTransactionsQueue = require('./messages').sendMessageToTransactionsQueue;
+const { winston } = require('../elasticsearch/winston');
+console.log('erfsdfsf3', sendMessageToTransactionsQueue.toString());
+// console.log(sendMessageToTransactionsQueue);
+// console.log(sendMessageToCashoutQueue);
+
 var db = require ('../database/databasePG.js');
 var dwolla = require ('../middleware/dwolla.js');
+const plaid = require('../middleware/plaid');
+
+var parseBodyOfMessage = function (message) {
+  try {
+    message = JSON.parse(message.Body);
+  } catch (err) {
+    message = (message);
+  }
+  return message;
+};
 
 
 var action = function (actionData) {
   var status;
-  console.log('route', actionData.route);
+  // console.log('teste');
+  actionData = parseBodyOfMessage(actionData);
+  // console.log('route', actionData);
+  // console.log('route', actionData.route);
+
+  winston.info({
+    transactionID: actionData.transactionID,
+    type: 'message',
+    stage: 'received',
+    route: actionData.route
+  });
   plaid.getAuth()
     .then (apiData =>{
       // console.log('api data', apiData)
@@ -23,6 +49,7 @@ var action = function (actionData) {
     })
     .then ((data) => {
       console.log(data);
+      // console.log('jsf;klsaj;flk', sendMessageToTransactionsQueue.toString() )
       return sendMessageToTransactionsQueue (actionData.transactionID, status);
     })
     .then ((data)=> {
@@ -52,7 +79,15 @@ var action = function (actionData) {
 
 
 var actionsForBankDeposits = function (data) {
-  console.log('route', actionData.route);
+  var data = actionData = parseBodyOfMessage(data);
+  // console.log('route', actionData.route);
+
+  winston.info({
+    trasactionID: actionData.transactionById,
+    type: 'confirmation',
+    stage: 'received from queue',
+  });
+
   dwolla.transferFunds(amount, processorToken)
     .then (transferResults => {
       var status;
@@ -61,8 +96,11 @@ var actionsForBankDeposits = function (data) {
       } else if (transferResults === false) {
         status = 'cancelled';
       }
-      db.updateTransactionStatus(transactionById, status)
-      sendMessageToTransactionsQueue(transactionById, status);
+      db.updateTransactionStatus(transactionById, status);
+      return sendMessageToTransactionsQueue(transactionById, status);
+    })
+    .then(data => {
+
     })
     .catch (err => {
       console.log(err);
