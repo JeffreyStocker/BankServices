dc5b6639414bcace96ecb7706558bcd5ea80c0bb
Jeff <jeffreyRstocker@gmail.com>
2018-01-04 10:51:26 -0800

    format: reorgnized messages.js

--- queues/messages.js
+++ queues/messages.js
@@ -1,36 +1,21 @@
+var { winston } = require ('../elasticsearch/winston');
+var { sqs } = require ('./awsinit.js');
+
 if (!process.env.PORT) {
   var dotenv = require('dotenv').config();
 }
-var fake = process.env.USEFAKE;
 
-
-// var Consumer = require('sqs-consumer'); //https://www.npmjs.com/package/sqs-consumer
-// var plaid = require ('../middleware/plaid.js');
-var { winston } = require ('../elasticsearch/winston');
-var { sqs } = require ('./awsinit.js');
-// var db = require('../database/databasePG.js');
-// SQS_URL = process.env.NODE_ENV === 'production' ? process.env.SQS_URL : process.env.SQS_MOCK_URL
-
-
-if (fake === 'false' || fake === false) {
+if (process.env.USEMOCKSQS === 'false') {
   console.log('Using Real SQS Queues');
-  var SQS_URL = process.env.SQS_URL;
-  var SQS_TRANSACTION_URL = 'https://sqs.us-east-2.amazonaws.com/722156248668/outputToTransactions';
-  var SQS_CASHOUT_URL = 'https://sqs.us-east-2.amazonaws.com/722156248668/cashout';
-  var SQS_BankServices = 'https://sqs.us-east-2.amazonaws.com/722156248668/inputToBankServices';
+  var SQS_TRANSACTION_URL = process.env.SQS_TRANSACTION_URL;
+  var SQS_CASHOUT_URL = process.env.SQS_CASHOUT_URL;
+  var SQS_BankServices = process.env.SQS_BankServices;
 } else {
   console.log('Using Mock SQS Queues');
-  var SQS_URL = 'http://localhost:8000/test';
   var SQS_TRANSACTION_URL = 'http://localhost:8000/outputToTransactions';
   var SQS_CASHOUT_URL = 'http://localhost:8000/cashout';
   var SQS_BankServices = 'http://localhost:8000/inputToBankServices';
 }
-
-// var AWS = require('aws-sdk');
-// AWS.config.update({accessKeyId: process.env.AWS_PUBLIC_KEY, secretAccessKey: process.env.AWS_SECRET_KEY});
-// var sqs = module.exports.sqs = new AWS.SQS({
-//   region: 'us-east-2',
-// });
 
 
 module.exports.sendMessageToTransactionsQueue = function (transactionID, status) {
@@ -40,7 +25,6 @@ module.exports.sendMessageToTransactionsQueue = function (transactionID, status)
       status: status //must be 'approved'/ 'declined' / 'cancelled' or 'confirmed'
     };
     var params = {
-      // MessageBody: JSON.stringify(msg),
       MessageBody: JSON.stringify(msg),
       QueueUrl: SQS_TRANSACTION_URL
     };
@@ -65,7 +49,7 @@ module.exports.sendMessageToCashoutQueue = function (transactionID, callback = (
       MessageBody: JSON.stringify(msg),
       QueueUrl: SQS_CASHOUT_URL
     };
-    sqs.sendMessage(params,(err, response) => {
+    sqs.sendMessage(params, (err, response) => {
       if (err) {
         revoke (err);
       } else {
